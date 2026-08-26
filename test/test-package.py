7c5da1c054be594f7c120961631917105a69ba72
Ubuntu <ubuntu@ip-172-31-35-161.ec2.internal>
2026-08-26 19:19:38 +0000

    actualizaciones en aws

--- test/test-package.py
+++ test/test-package.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from model.predict import make_prediction
 
-sample_input_data = pd.read_csv("~/test/bankchurn_test.csv")
+sample_input_data = pd.read_csv("~/mlops-taller5/test/bankchurn_test.csv")
 result = make_prediction(input_data=sample_input_data)
 print(result)
